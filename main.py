c3e0ddcdd87c809942573ab76ffa6bd5b2df573d
zshawar <zshawar@uncc.edu>
2021-07-31 17:44:36 -0400

    imported tkinter and created a sized window with a background color

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+# create window
+root=Tk()
+
+# use geometry method to create width and height of window and for distance from x and y axis
+root.geometry('500x570+100+30')
+
+# add title to window
+root.title('ChatBot created by Zaina Shawar')
+
+# add background color
+# root.config(bg='lavender')
+root['bg'] = '#f3c9ff'
+
+# use mainloop to keep window continuously on screen
+root.mainloop()
+
+
